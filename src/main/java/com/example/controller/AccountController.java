6f669d742333d15fd53a3590b5498618ec93d3d4
MitsuruYakushinji <141882459+MitsuruYakushinji@users.noreply.github.com>
2023-12-11 08:20:55 +0900

    Lesson2
    預金機能まで実装

--- src/main/java/com/example/controller/AccountController.java
+++ src/main/java/com/example/controller/AccountController.java
@@ -1,11 +1,20 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.entity.Account;
+import com.example.resource.RequestAmount;
+import com.example.resource.ResponseAmount;
 import com.example.service.AccountService;
 
 @RestController
+@RequestMapping(value = "/account", produces="application/json;charset=UTF-8")
 public class AccountController {
 
 	private final AccountService accountService;
@@ -14,4 +23,61 @@ public class AccountController {
 	public AccountController(AccountService accountService) {
 		this.accountService = accountService;
 	}
+	
+	@PostMapping("bankTrading/open")
+	public Account open() {
+		Account account = new Account();
+		account.setAmount(0);
+		
+		return this.accountService.save(account.getAmount());
+	}
+	
+	@GetMapping("bankTrading/{account_id}")
+	public ResponseAmount getAmount(@PathVariable("account_id")Integer accountId) {
+		
+		// 取得した口座IDから対象口座を取得する
+		Account account = this.accountService.findById(accountId);
+		
+		// ResponseAmountインスタンスを生成
+		ResponseAmount responseAmount = new ResponseAmount();
+		
+		// 対象口座の預金額をresponseAmount.amountにセットする
+		responseAmount.setAmount(account.getAmount());
+		
+		return responseAmount;
+	}
+	
+	@PostMapping("bankTrading/deposit/{account_id}")
+	public ResponseAmount deposit(@PathVariable("account_id")Integer accountId, @RequestBody RequestAmount requestAmount) {
+		
+		// 取得した口座IDから対象口座を取得する
+		Account account = this.accountService.findById(accountId);
+		
+		ResponseAmount responseAmount = new ResponseAmount();
+		
+		Integer addAmount = account.getAmount() + requestAmount.getAmount();
+		
+		responseAmount.setAmount(addAmount);
+		
+		return responseAmount;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,8 +1,11 @@
 package com.example.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.entity.Account;
 import com.example.repository.AccountRepository;
 
 @Service
@@ -14,5 +17,24 @@ public class AccountService {
 	// コンストラクタインジェクション
 	public AccountService(AccountRepository accountRepository) {
 		this.accountRepository = accountRepository;
+	}
+	
+	public Account save(Integer amount) {
+		Account account = new Account();
+		
+		// 
+		account.setAmount(amount);
+		
+		return this.accountRepository.save(account);
+	}
+	
+	public Account findById(Integer id) {
+		
+		// IDカラムでデータ検索するメソッド
+		Optional<Account> optionalAccount = this.accountRepository.findById(id);
+			
+		Account account = optionalAccount.get();
+		
+		return account;
 	}
 }
