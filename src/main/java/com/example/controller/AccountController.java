a3d413ae778e4b7218e521dfae883039d1d70c45
MitsuruYakushinji <141882459+MitsuruYakushinji@users.noreply.github.com>
2023-12-11 16:51:03 +0900

    Lesson2 解答付き

--- src/main/java/com/example/controller/AccountController.java
+++ src/main/java/com/example/controller/AccountController.java
@@ -14,7 +14,12 @@ import com.example.resource.ResponseAmount;
 import com.example.service.AccountService;
 
 @RestController
-@RequestMapping(value = "/account", produces="application/json;charset=UTF-8")
+/*
+ * 自分の回答
+ * @RequestMapping(value = "/account", produces="application/json;charset=UTF-8")
+ *
+ */
+@RequestMapping("/bankTrading")
 public class AccountController {
 
 	private final AccountService accountService;
@@ -24,17 +29,40 @@ public class AccountController {
 		this.accountService = accountService;
 	}
 	
-	@PostMapping("bankTrading/open")
-	public Account open() {
-		Account account = new Account();
-		account.setAmount(0);
-		
-		return this.accountService.save(account.getAmount());
-	}
+    /**
+     * 課題1. 口座開設
+     *
+     * 新規口座を開設する
+     *
+     * @return Account 新規登録した口座情報
+     * 
+     * 自分の回答
+     * 	@PostMapping("bankTrading/open")
+     * 	public Account open() {
+     * 	Account account = new Account();
+     * 	account.setAmount(0);
+     * 	return this.accountService.save(account.getAmount());
+     * 	}
+     * 
+     */
 	
-	@GetMapping("bankTrading/{account_id}")
-	public ResponseAmount getAmount(@PathVariable("account_id")Integer accountId) {
-		
+    @PostMapping("/open")
+    public Account open() {
+        return accountService.createAccount();
+    }
+	
+    /**
+     * 課題2. 残高照会
+     *
+     * 口座IDに紐づく残高を取得する
+     *
+     * @param accountId 口座ID
+     * @return ResponseAmount 残高のレスポンス
+     * 
+     * 自分の回答
+     * 
+     * 	@GetMapping("bankTrading/{account_id}")
+     * 	public ResponseAmount getAmount(@PathVariable("account_id")Integer accountId) {
 		// 取得した口座IDから対象口座を取得する
 		Account account = this.accountService.findById(accountId);
 		
@@ -45,10 +73,27 @@ public class AccountController {
 		responseAmount.setAmount(account.getAmount());
 		
 		return responseAmount;
-	}
+		}
+     */
+    
+    @GetMapping("/{account_id}")
+    public ResponseAmount getAmount(@PathVariable("account_id") Integer accountId) {
+        return accountService.getResponseAmount(accountId);
+    }
 	
-	@PostMapping("bankTrading/deposit/{account_id}")
-	public ResponseAmount deposit(@PathVariable("account_id")Integer accountId, @RequestBody RequestAmount requestAmount) {
+    /**
+     * 課題3. 預け入れ
+     *
+     * 預金後の残高を返却する
+     *
+     * @param accountId 口座ID
+     * @param requestAmount 預金額
+     * @return ResponseAmount 残高のレスポンス
+     * 
+     * 自分の回答
+     * 
+     * 	@PostMapping("bankTrading/deposit/{account_id}")
+     * 	public ResponseAmount deposit(@PathVariable("account_id")Integer accountId, @RequestBody RequestAmount requestAmount) {
 		
 		// 取得した口座IDから対象口座を取得する
 		Account account = this.accountService.findById(accountId);
@@ -60,10 +105,27 @@ public class AccountController {
 		responseAmount.setAmount(addAmount);
 		
 		return responseAmount;
-	}
+		}
+     */
+    
+    @PostMapping("/deposit/{account_id}")
+    public ResponseAmount deposit(@PathVariable("account_id") Integer accountId, @RequestBody RequestAmount requestAmount) {
+        return accountService.depositAccount(accountId, requestAmount);
+    }
 	
-	@PostMapping("bankTrading/withdraw/{account_id}")
-	public ResponseAmount withdraw(@PathVariable("account_id")Integer accountId, @RequestBody RequestAmount requestAmount) {
+    /**
+     * 課題4. 引き出し
+     *
+     * 引出後の残高を返却する
+     *
+     * @param accountId 口座ID
+     * @param requestAmount 引出額
+     * @return ResponseAmount 残高のレスポンス
+     * 
+     * 自分の回答
+     * 
+     * 	@PostMapping("bankTrading/withdraw/{account_id}")
+     * 	public ResponseAmount withdraw(@PathVariable("account_id")Integer accountId, @RequestBody RequestAmount requestAmount) {
 		
 		// 取得した口座IDから対象口座を取得する
 		Account account = this.accountService.findById(accountId);
@@ -77,7 +139,12 @@ public class AccountController {
 		}
 		
 		return responseAmount;
-	}
+		}
+     */
+    @PostMapping("/withdraw/{account_id}")
+    public ResponseAmount withdraw(@PathVariable("account_id") Integer accountId, @RequestBody RequestAmount requestAmount) {
+        return accountService.withdrawAccount(accountId, requestAmount);
+    }
 }
 
 
